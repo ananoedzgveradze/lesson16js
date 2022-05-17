967b450e517c24ab6d9068100ba25e81418560fd
ananoedzgveradze <102252416+ananoedzgveradze@users.noreply.github.com>
2022-05-17 16:23:27 +0400

    add java

--- java.js
+++ java.js
@@ -1,43 +1,44 @@
-// ajax request.
-//1.xml http requist
-//2.fetch
+// // ajax request.
+
+// //1.xml http request
+// //2.fetch
 
 
-function getUsers() {
+// function getUsers() {
 
-}
-let request = new XMLHttpRequest();
-request.addEventListener('load', render);
+// }
+// let request = new XMLHttpRequest();
+// request.addEventListener('load', render);
     
 
 
-request.open('GET', 'https://reqres.in/api/users?page=2');
+// request.open('GET', 'https://reqres.in/api/users?page=2');
 
-request.send();
+// request.send();
 
-function render () {
- let response = this.responseText;
- let responseData = JSON.parse(response);
+// function render () {
+//  let response = this.responseText;
+//  let responseData = JSON.parse(response);
 
-//  //let ul = document.createElement('ul');
+// //  //let ul = document.createElement('ul');
 
-//  responseData.data.forEach(item => {
-//      let li = document.createElement('li');
-//      li.textContent = item.email;
-//      li.classList.add('li-item');
+// //  responseData.data.forEach(item => {
+// //      let li = document.createElement('li');
+// //      li.textContent = item.email;
+// //      li.classList.add('li-item');
 
-//      ul.appendChild(li);
+// //      ul.appendChild(li);
 
-//  });
+// //  });
 
-let ul = document.createElement('ul');
-let li = document.createElement('li');
-li.textContent = responseData.data[2].email;
-ul.appendChild(li);
+// let ul = document.createElement('ul');
+// let li = document.createElement('li');
+// li.textContent = responseData.data[2].email;
+// ul.appendChild(li);
 
- document.getElementById('api-user-email').appendChild(ul);
+//  document.getElementById('api-user-email').appendChild(ul);
 
- console.log (responseData);
-}
+//  console.log (responseData);
+// }
 
-getUsers();
+// getUsers();
