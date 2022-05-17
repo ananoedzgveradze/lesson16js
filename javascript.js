76d7a20f334d65346edfe8cf3164c06376582519
ananoedzgveradze <102252416+ananoedzgveradze@users.noreply.github.com>
2022-05-17 16:51:04 +0400

    add script

--- javascript.js
+++ javascript.js
@@ -4,17 +4,13 @@ let totalPagesApi;
 function getUsers(page) {
     fetch('https://reqres.in/api/users?page=' + page, {
         method: 'GET'
-    })
-
-
-.then(function (response) {
+    }).then(function (response) {
     if (response.status !== 200) {
         throw response.status;
     }
     return response. json();
-})
-
-.then(function (responseData) {
+}).then(function (responseData) {
+    console.log(responseData);
     var fragment = document.createDocumentFragment();
     responseData.data.forEach(item => {
         let li = document.createElement('li');
@@ -28,21 +24,20 @@ function getUsers(page) {
 
         li.appendChild(imgUser);
         li.appendChild(pEmail);
-        li.appendChild('li-item');
+        // li.appendChild(document.createElement('li-item'));
         fragment.appendChild(li);
     });
     document.getElementById('ul-list').innerHTML = '';
     document.getElementById('ul-list').appendChild(fragment);
 
     totalPagesApi = responseData.total_pages;
-})
-
-.catch(function (error) {
+}).catch(function (error) {
     if (error == 404) {
         let p = document.createElement('p');
         p.textContent='server error';
         document.getElementById('api-user-email').appendChild(p);
     } else {
+        console.log(error)
         console.log('Page is not Found');
     }
     
@@ -56,6 +51,12 @@ document.getElementById('loadprev').addEventListener('click',function () {
         return;
     }
     currentPage -=1;
+    getUsers(currentPage);
+});
+
+document.getElementById('loadnext').addEventListener('click',function () {
+    currentPage +=1;
+    console.log(currentPage);
     getUsers(currentPage);
 })
 
